bc28b2055d3208579a67a319eff7f2b0bf5c7afb
aaronchen <tks35881@gmail.com>
2021-07-23 20:41:30 +0800

    實作夜間模式

--- app/src/main/java/com/aaron/todolist_android/MainActivity.kt
+++ app/src/main/java/com/aaron/todolist_android/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.aaron.todolist_android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,12 +12,14 @@ import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+   //private var mSysThemeConfig = -1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment)  as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
             when(destination.id){
                 R.id.mainFragment ->{
@@ -39,6 +44,36 @@ class MainActivity : AppCompatActivity() {
             true
         }
         toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        processToolBarStatus()
     }
 
+    private fun updateUiMode(){
+//        when (mSysThemeConfig) {
+//            Configuration.UI_MODE_NIGHT_NO -> {
+//                //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+//            }
+//            Configuration.UI_MODE_NIGHT_YES -> {
+//                //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//            }
+//        }
+        recreate()
+    }
+
+    private fun processToolBarStatus(){
+        when(navController.currentDestination?.id){
+            R.id.mainFragment ->{
+                toolbar.menu.setGroupVisible(0,true)
+            }
+            else -> {
+                toolbar.menu.setGroupVisible(0,false)
+            }
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        //mSysThemeConfig = newConfig.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        updateUiMode()
+    }
 }
--- app/src/main/java/com/aaron/todolist_android/page/SettingsFragment.kt
+++ app/src/main/java/com/aaron/todolist_android/page/SettingsFragment.kt
@@ -1,11 +1,15 @@
 package com.aaron.todolist_android.page
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.aaron.todolist_android.R
+import kotlinx.android.synthetic.main.fragment_settings.*
+import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 class SettingsFragment: Fragment() {
     override fun onCreateView(
@@ -13,10 +17,39 @@ class SettingsFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_settings,container,false)
+        val view = inflater.inflate(R.layout.fragment_settings,container,false)
+        checkUiModeStatus(view)
+        return view
+    }
+
+    private fun checkUiModeStatus(view: View){
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_NO -> {
+                view.theme_switch.isChecked = false
+            }
+            Configuration.UI_MODE_NIGHT_YES -> {
+                view.theme_switch.isChecked = true
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        theme_switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
+
+//    override fun onConfigurationChanged(newConfig: Configuration) {
+//        super.onConfigurationChanged(newConfig)
+//        view?.let { checkUiModeStatus(it) }
+//    }
+
+
+
 }
